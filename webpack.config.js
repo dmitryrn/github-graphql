6733e211e480fea973ea3a8c9eb26b9143469bda
Dmitry R <dimanius851@gmail.com>
2020-03-10 18:46:32 +0300

    add babel and ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,5 +11,16 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?[jt]sx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 }

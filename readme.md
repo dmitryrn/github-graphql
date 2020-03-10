9123789737caac49aaf37f09826742b522e4db13
Dmitry R <dimanius851@gmail.com>
2020-03-10 19:36:47 +0300

    add relay

---
+++ readme.md
@@ -0,0 +1,7 @@
+brew install watchman
+
+npm i
+
+npm run relay:watch
+
+npm start
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {graphql, QueryRenderer} from 'react-relay'
+
+import environment from './relay'
 
 const App = () => {
-  return <div>test</div>
+  return  <QueryRenderer
+    environment={environment}
+    query={graphql`
+      query srcTestQuery {
+        country(code: "US") {
+          name
+        }
+      }
+    `}
+    variables={{}}
+    render={({error, props}) => {
+      if (error) {
+        return <div>Error!</div>
+      }
+      if (!props) {
+        return <div>Loading...</div>
+      }
+      return <div>User ID: {props.viewer.id}</div>
+    }}
+  />
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))

89bce9cbb750c13a4478ebbf7fcdbd3d5f3534f3
Dmitry R <dimanius851@gmail.com>
2020-03-29 18:42:09 +0300

    persist token in local storage

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -1,7 +1,10 @@
 import { createStore, createEvent } from 'effector'
 
-export const $token = createStore<string | null>(null)
+export const $token = createStore<string | null>(localStorage.getItem('token'))
 
 export const setToken = createEvent<string>()
 
-$token.on(setToken, (_, payload) => payload)
+$token.on(setToken, (_, payload) => {
+  localStorage.setItem('token', payload)
+  return payload
+})

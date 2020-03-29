6abd22a7694f02569dd01b575d0eb0aefaf8844f
Dmitry R <dimanius851@gmail.com>
2020-03-29 18:42:48 +0300

    add repositories load and view logic

--- packages/client/src/repos.tsx
+++ packages/client/src/repos.tsx
@@ -1,20 +1,77 @@
-import React, { useEffect } from 'react'
-import { gql, fromPromise } from 'apollo-boost'
+import React from 'react'
+import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
+import produce from 'immer'
 
-import { me } from './__generated__/me'
+import * as Repositories from './__generated__/repositories'
 
 const query = gql`
-  query me {
+  query repositories($after: String) {
     viewer {
-      name
-      bio
+      repositories(after: $after, first: 5) {
+        nodes {
+          id
+          name
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
     }
   }
 `
 
 export const Repos = () => {
-  const { loading, error, data } = useQuery<me>(query)
+  const { loading, error, data, fetchMore } = useQuery<
+    Repositories.repositories,
+    Repositories.repositoriesVariables
+  >(query, {
+    variables: {
+      after: null
+    }
+  })
 
-  return <div>{loading ? 'loa' : data?.viewer.name}</div>
+  if (loading) {
+    return <div>loading</div>
+  }
+
+  if (!loading && !data) {
+    return <div>nothing was received from server</div>
+  }
+
+  if (error) {
+    console.log(error)
+    return <div>error. check console</div>
+  }
+  console.log(data)
+  return (
+    <div>
+      <p>public repositories:</p>
+      {data?.viewer.repositories.nodes?.map((repo) => (
+        <div key={repo?.id}>{repo?.name}</div>
+      ))}
+      {data?.viewer.repositories.pageInfo.hasNextPage && (
+        <button
+          onClick={() => {
+            fetchMore({
+              variables: {
+                after: data.viewer.repositories.pageInfo.endCursor
+              },
+              updateQuery: (prevRes, { fetchMoreResult }) =>
+                fetchMoreResult
+                  ? produce(fetchMoreResult, (draft) => {
+                      draft.viewer.repositories.nodes = prevRes.viewer.repositories.nodes!.concat(
+                        draft!.viewer.repositories.nodes
+                      )
+                    })
+                  : prevRes
+            })
+          }}
+        >
+          load more
+        </button>
+      )}
+    </div>
+  )
 }
